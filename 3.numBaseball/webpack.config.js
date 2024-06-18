816db676dbb7dd56cb004ba66b935c7ca451f8cc
Yoon <151509222+yoonB-dev@users.noreply.github.com>
2024-06-18 23:55:16 +0900

    WebGame

---
+++ 3.numBaseball/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry:{
+        app:"./main.js",
+
+    },
+    module:{
+        rules:[{
+            
+        }],
+    },
+    plugins:[],
+    output:{
+        filename:"[name].js",
+        path:'./dist',
+    },
+};
